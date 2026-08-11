b51ceb0da5038c35f3014cb5cb1492b2e472ee5c
TuanKiet <123tuankiet321@gmail.com>
2026-08-11 15:16:15 +0700

    Init project

---
+++ back-end/enrollment-service/src/test/java/com/elearning/enrollment/EnrollmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elearning.enrollment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnrollmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ back-end/payment-service/src/test/java/com/elearning/payment/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elearning.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
